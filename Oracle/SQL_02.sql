402039c3994bd8715f25d54a8398167393bdd04f
SungWoo0315 <noel7752@gmail.com>
2021-06-14 14:13:36 +0900

    Update SQL_02.sql

--- Oracle/SQL_02.sql
+++ Oracle/SQL_02.sql
@@ -1,4 +1,4 @@
-
+﻿
 
 
 create table dept(
@@ -7,7 +7,13 @@ create table dept(
     ,loc         varchar2(20)    not null
 );
 
-select * from dept;
+insert into dept(dep_no, dep_name, loc) values(10, '총무부', '서울');
+insert into dept(dep_no, dep_name, loc) values(20, '영업부', '부산');
+insert into dept(dep_no, dep_name, loc) values(30, '전산부', '대전');
+insert into dept(dep_no, dep_name, loc) values(40, '자재부', '광주');
 
+commit;
+
+select * from dept;
 
 drop table dept;
