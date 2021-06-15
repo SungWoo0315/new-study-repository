06ca28db947204e967080b0ba6683759337cbb85
SungWoo0315 <noel7752@gmail.com>
2021-06-15 09:59:08 +0900

    Update SQL_01.sql

--- Oracle/SQL_01.sql
+++ Oracle/SQL_01.sql
@@ -1,5 +1,6 @@
+﻿
 
+create user king identified by k123;   -- king 계정 생성 쿼리문.
 
-create user king identified by k123;
+grant connect,resource,dba to king;    -- 계정 권한 부여 쿼리문.
 
-grant connect,resource,dba to king;
