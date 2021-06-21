074c8094611f4ba24723bc1624b8524c96f03b33
SungWoo0315 <noel7752@gmail.com>
2021-06-21 12:19:41 +0900

    Update SQL_0621.sql

--- Oracle/SQL_0621.sql
+++ Oracle/SQL_0621.sql
@@ -129,13 +129,51 @@ select
 	,c.cus_name	"담당고객명"
 	,c.tel_num "고객전화번호"
 from
-	 employee e inner join dept d inner join customer c
-on
-	e.dep_no=d.dep_no and c.emp_no=c.emp_no;
+	 (employee e inner join dept d on e.dep_no=d.dep_no)
+	 inner join customer c on c.emp_no=e.emp_no
+order by
+	e.emp_name asc;
 
 
 
 
+--<95>
+select
+	e.emp_name	"직원명"
+	,e.jikup	"직원직급"
+	,d.dep_name	"소속부서명"
+	,s.sal_grade_no "연봉등급"
+from
+	 employee e, dept d, salary_grade s
+where
+	e.dep_no=d.dep_no and (e.salary>=s.min_salary and e.salary<=s.max_salary)
+order by
+	s.sal_grade_no asc
+	,decode(e.jikup, '사장',1,'부장',2, '과장',3, '대리',4,'주임',5,6) asc
+	,	case
+			when substr(e.jumin_num,7,1) in('1','2') then '19'
+			else '20'
+		end || substr(e.jumin_num,1,6)
+		asc;
+
+
+--ANSI
+select
+  e.emp_name	"직원명"
+  ,e.jikup	"직원직급"
+  ,d.dep_name	"소속부서명"
+  ,s.sal_grade_no "연봉등급"
+from
+	 (employee e inner join dept d on e.dep_no=d.dep_no)
+	 inner join salary_grade s on (e.salary>=s.min_salary and e.salary<=s.max_salary)
+order by
+	s.sal_grade_no asc
+	,decode(e.jikup, '사장',1 ,'부장',2, '과장',3, '대리',4,'주임',5,6) asc
+	,	case
+		when substr(e.jumin_num,7,1) in('1','2') then '19'
+		else '20'
+	end || substr(jumin_num,1,6)
+	asc;
 
 
 
