0d30516c2c23462c72baab1ee9d8b21973253850
SungWoo0315 <noel7752@gmail.com>
2021-06-21 17:57:45 +0900

    Update SQL_0621.sql

--- Oracle/SQL_0621.sql
+++ Oracle/SQL_0621.sql
@@ -267,11 +267,145 @@ from
 
 
 
+--<99>
+select
+	c.cus_name	"고객명"
+	,c.tel_num	"고객전화번호"
+	,e.emp_name	"담당직원명"
+	,e.jikup	"담당직원직급"
+	,e.dep_no "부서번호"
+from
+	customer c, employee e
+where
+	c.emp_no=e.emp_no(+);
+
+
+--<100>
+select
+	c.cus_no "고객번호"
+	,c.cus_name	"고객명"
+	,c.tel_num	"고객전화번호"
+	,e.emp_name	"담당직원명"
+	,e.jikup	"담당직원직급"
+	,e.dep_no "부서번호"
+from
+	customer c, employee e
+where
+	c.emp_no = e.emp_no(+)
+	and e.dep_no(+) = 10
+	;
+
+-------------------------------------------------
+--ANSI 조인
+-------------------------------------------------
+select
+	c.cus_no "고객번호"
+	,c.cus_name	"고객명"
+	,c.tel_num	"고객전화번호"
+	,e.emp_name	"담당직원명"
+	,e.jikup	"담당직원직급"
+	,e.dep_no "부서번호"
+from
+	customer c left outer join employee e
+on
+	c.emp_no = e.emp_no and	e.dep_no = 10;
 
 
 
 
+--<101>
 
+select
+	c.cus_no "고객번호"
+	,c.cus_name	"고객명"
+	,c.tel_num	"고객전화번호"
+	,e.emp_name	"담당직원명"
+	,e.jikup	"담당직원직급"
+	,s.sal_grade_no "담당직원 연봉등급"
+from
+	customer c, employee e, salary_grade s
+where
+	c.emp_no = e.emp_no(+)
+	--and e.salary(+) between s.min_salary and s.max_salary;
+    and(e.salary>=s.min_salary(+) and e.salary<=s.max_salary(+))
+--내가한것.
+
+--정답.
+select
+	c.cus_no "고객번호"
+	,c.cus_name	"고객명"
+	,c.tel_num	"고객전화번호"
+	,e.emp_name	"담당직원명"
+	,e.jikup	"담당직원직급"
+	,s.sal_grade_no "담당직원 연봉등급"
+from
+	customer c, employee e, salary_grade s
+
+where
+	c.emp_no = e.emp_no(+)
+	--and e.salary between s.min_salary(+) and s.max_salary(+);
+    and s.min_salary(+) <= e.salary and s.max_salary(+) >= e.salary
+
+ order by
+    c.cus_no asc;
+
+
+
+--<102>
+select
+	c.cus_no	"고객번호"
+	,c.cus_name "고객명"
+	,c.tel_num	"고객전화번호"
+
+from
+	customer c, employee e
+where
+	e.salary>=3000 and e.emp_no=c.emp_no
+
+
+
+--<103>
+
+select
+	c.cus_no	"고객번호"
+	,c.cus_name "고객명"
+	,c.jumin_num	"고객주민번호"
+
+from
+	customer c, employee e
+where
+	to_number(to_char(sysdate, 'YYYY'))
+	- to_number(
+		case substr(e.jumin_num,7,1) when '1' then '19' when '2' then '19' else '20' end
+		||substr(e.jumin_num,1,2)+1) >= 40 and e.emp_no=c.emp_no;
+
+
+--<104>
+select
+	nvl(c.cus_no||'','없음')	"고객번호"
+	,nvl(c.cus_name,'없음') "고객명"
+	,nvl(e.emp_no||'','없음')	"담당직원번호"
+	,nvl(e.emp_name,'없음') "담당직원명"
+	,nvl(d.dep_name,'없음') "담당직원소속부서명"
+	,nvl(s.sal_grade_no||'','없음') "담당직원연봉등급"
+	,nvl(e2.emp_name,'없음') "담당직원직속상관명"
+	,nvl(e2.jikup,'없음') "담당직원직속상관직급"
+	,nvl(s2.sal_grade_no||'','없음') "직속상관연봉등급"
+	--,nvl(c.cus_name||'','없음')
+from
+	customer c, employee e, dept d, salary_grade s, employee e2, salary_grade s2
+where
+	c.emp_no=e.emp_no(+)
+	and
+	e.dep_no=d.dep_no(+)
+	and
+	e.emp_no=e2.mgr_emp_no(+)
+	and
+	s.min_salary(+) <= e.salary and s.max_salary(+) >= e.salary
+	and
+	s2.min_salary(+) <= e2.salary and s2.max_salary(+) >= e2.salary
+order by
+ c.cus_no asc
 
 
 
