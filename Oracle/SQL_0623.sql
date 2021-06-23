90973ddfc48006dd00d92bf713ce09672e77c70f
SungWoo0315 <noel7752@gmail.com>
2021-06-23 17:09:47 +0900

    Update SQL_0623.sql

--- Oracle/SQL_0623.sql
+++ Oracle/SQL_0623.sql
@@ -400,14 +400,98 @@ group by
 
 --<135>
 
+select
+	to_char(hire_date, 'MM')||'월' "입사월"
+	,count(emp_no)||'명' "입사인원수"
+from
+	employee
+group by
+	to_char(hire_date, 'MM')||'월'
+order by
+	"입사월";
+
+
+
 
 --<136>
+select
+	jikup                           "직급"
+	,round(avg(salary),1)           "직급평균연봉"
+	,count(*)||'명'   "인원수"
+from
+	employee
+group by
+	jikup
+order by decode( jikup,'사장', 1,'부장', 2,'과장', 3,'대리', 4,'주임', 5, 6)
 
 
 --<137>
 
+select
+	d.dep_no          "부서번호"
+	,d.dep_name       "부서명"
+	,count(distinct(e.emp_no))  "직원수"
+	,count(c.emp_no)           "관리고객수"
+from
+	employee e, dept d, customer c
+where
+	d.dep_no = e.dep_no(+)
+	and
+	e.emp_no = c.emp_no(+)
+group by
+	d.dep_no, d.dep_name;
+
+
 
 --<138>
+select
+	e1.emp_no                                "직원번호"
+	,e1.emp_name                             "직원명"
+	,ceil((sysdate - e1.hire_date)/365)||'년차' "근무년차"
+	,60
+	- (extract(year from sysdate)
+	- to_number(decode(substr(e1.jumin_num,7,1),'1','19','2','19','20')||substr(e1.jumin_num,1,2)) )+1                                        "퇴직일까지 남은 년도"
+	,case when
+			to_date(
+				to_char(sysdate,'YYYY')||substr(e1.jumin_num,3,4)
+				, 'YYYYMMDD'
+			)
+			-
+			sysdate
+			>=0
+		then	--올해 생일을 년-월-일 로 리턴하는 식
+			to_char(
+					to_date(
+							to_char(sysdate,'YYYY')||substr(e1.jumin_num,3,4)
+							, 'YYYYMMDD'
+					)
+					,'YYYY-MM-DD (dy)' , 'NLS_DATE_LANGUAGE = Korean'
+			)
+		else  --내년 생일을 년-월-일 로 리턴하는 식
+			to_char(
+					to_date(
+							to_number(to_char(sysdate,'YYYY'))+1||substr(e1.jumin_num,3,4)
+							, 'YYYYMMDD'
+					)
+					,'YYYY-MM-DD (dy)' , 'NLS_DATE_LANGUAGE = Korean'
+			)
+	end                                      "생일"
+	,d1.dep_name                             "소속부서명"
+	,e2.emp_name                             "직속상관명"
+	,d2.dep_name                             "직속상관부서명"
+from
+	employee e1, dept d1, employee e2, dept d2
+where
+	e1.dep_no = d1.dep_no(+)
+	and e1.mgr_emp_no = e2.emp_no(+)
+	and e2.dep_no = d2.dep_no(+)
+order by
+	decode( e1.jikup,'사장', 1,'부장', 2,'과장', 3,'대리', 4,'주임', 5, 6)||decode(substr(e1.jumin_num,7,1),'1','19','2','19','20') ||substr(e1.jumin_num,1,6);
+
+
+
+
+
 
 
 --<139>
