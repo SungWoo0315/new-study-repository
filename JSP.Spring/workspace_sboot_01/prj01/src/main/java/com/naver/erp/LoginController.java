e094893f5c73992009b4a53eba32781f4bac579e
SungWoo0315 <noel7752@gmail.com>
2021-08-19 15:54:30 +0900

    Update LoginController.java

--- JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/LoginController.java
+++ JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/LoginController.java
@@ -1,5 +1,7 @@
 package com.naver.erp;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,4 +30,34 @@ public class LoginController {
         return mav;
 
     }
+    // ********************************************
+    // 가상주소 /loginProc.do 로 접근하면 호출되는 메소드 선언
+    // ********************************************
+    @RequestMapping( value="/loginProc.do")
+    public ModelAndView loginProc( 
+        // 클라이언트가 보낸 요청 메시지를 관리하는 HttpServletRequest 객체가 매개변수로 들어온다.
+        // HttpServletRequest 객체의 메소드를 이용하면 파라미터값을 얻을 수 있다.
+        HttpServletRequest request
+    ){
+        // --------------------------------------------------
+        // 클라이언트가 보낸 요청 메시지 안의 "id" 라는 파라미터명의 파라미터값 꺼내기
+        // 클라이언트가 보낸 아이디를 꺼내라
+        // --------------------------------------------------
+        String id = request.getParameter("id");
+        // --------------------------------------------------
+        // 클라이언트가 보낸 요청 메시지 안의 "pwd" 라는 파라미터명의 파라미터값 꺼내기
+        // 클라이언트가 보낸 암호를 꺼내라
+        // --------------------------------------------------
+        String pwd = request.getParameter("pwd");
+
+        // ---------------------------
+        // [ModelAndView 객체] 생성하기.
+        // [ModelAndView 객체] 에 [호출 JSP 페이지명]을 저장하기
+        // [ModelAndView 객체] 리턴하기.
+        // ---------------------------
+        ModelAndView mav = new ModelAndView();
+        return mav;
+
+    }
+
 }
