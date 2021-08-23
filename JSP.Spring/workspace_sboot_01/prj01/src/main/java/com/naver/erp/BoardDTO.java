4f7f31fb78de2581ef0976bd6a64bfe18d5987a9
SungWoo0315 <noel7752@gmail.com>
2021-08-23 14:53:07 +0900

    Update BoardDTO.java

--- JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/BoardDTO.java
+++ JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/BoardDTO.java
@@ -2,7 +2,17 @@ package com.naver.erp;
 
 public class BoardDTO {
 	
-	
+	b_no
+    subject
+    writer
+    reg_date
+    readcount
+    content
+    pwd
+    email
+    group_no
+    print_no
+    print_level
 	
 
 }
