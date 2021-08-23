2045898040892aa25d754a2166777c7828ff6f46
SungWoo0315 <noel7752@gmail.com>
2021-08-23 13:16:38 +0900

    Update BoardController.java

--- JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/BoardController.java
+++ JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/BoardController.java
@@ -66,4 +66,45 @@ public class BoardController {
 
     }
 
+
+    // mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    // 가상주소 /boardRegProc.do 로 접근하면 호출되는 메소드 선언
+    // mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    @RequestMapping( value="/boardRegProc.do")
+    public ModelAndView boardRegProc( 
+        
+        HttpServletRequest request
+
+    ){
+
+        String writer = request.getParameter("writer");
+        String subject = request.getParameter("subject");
+        String email = request.getParameter("email");
+        String content = request.getParameter("content");
+        String pwd = request.getParameter("pwd");
+        
+        System.out.println("==============================================\r ");
+        System.out.println("BoardController.boardRegProc.do => 파라미터명 writer => " + writer);
+        System.out.println("LoginController.boardRegProc.do => 파라미터명 subject => " + subject);
+        System.out.println("LoginController.boardRegProc.do => 파라미터명 email => " + email);
+        System.out.println("LoginController.boardRegProc.do => 파라미터명 content => " + content);
+        System.out.println("LoginController.boardRegProc.do => 파라미터명 pwd => " + pwd);
+        System.out.println("==============================================\r ");
+
+        // ***************************************
+        // [ModelAndView 객체] 생성하기
+        // ***************************************
+        ModelAndView mav = new ModelAndView();
+        // ***************************************
+        // [ModelAndView 객체] 에 [호출 JSP 페이지명]을 저장하기
+        // ***************************************
+        mav.setViewName("boardList.jsp");
+        // ***************************************
+        // [ModelAndView 객체] 리턴하기
+        // ***************************************
+        return mav;
+
+
+    }
+
 }
