a83ae07dfcd4771abfb70a5e45195b9545fa005c
SungWoo0315 <noel7752@gmail.com>
2021-08-24 03:29:57 +0900

    Update BoardServiceImpl.java

--- JSP.Spring/workspace_sboot_02/prj01/src/main/java/com/naver/erp/BoardServiceImpl.java
+++ JSP.Spring/workspace_sboot_02/prj01/src/main/java/com/naver/erp/BoardServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class BoardServiceImpl {
+public class BoardServiceImpl implements BoardService{
 
 	
 	// ****************************************************
