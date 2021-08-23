79bb2daa1713848e12c7ca9e407c73aed94ebdf6
SungWoo0315 <noel7752@gmail.com>
2021-08-23 11:37:34 +0900

    Create BoardController.java

---
+++ JSP.Spring/workspace_sboot_01/prj01/src/main/java/com/naver/erp/BoardController.java
@@ -0,0 +1,24 @@
+package com.naver.erp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+// ---------------------------------------------------------------
+// URL 주소로 접속하면 호출되는 메소드를 소유한 [컨트롤러 클래스] 선언
+// @Controller 를 붙임으로써 [컨트롤러 클래스]임을 지정한다.
+// ---------------------------------------------------------------
+@Controller
+public class BoardController {
+
+ 
+
+}
